678f3b2f32c8f7274c5acca6243b41b08cb5870e
BlueBird931 <lore.framarin@gmail.com>
2025-05-15 13:13:08 +0200

    added coordinates to meters conversion (to fix)

--- src/lib/gps/gps_trace.cpp
+++ src/lib/gps/gps_trace.cpp
@@ -1,1 +1,97 @@
 #include "gps_trace.hpp"
+
+namespace fcpp {
+
+namespace gps {
+
+gps_trace::gps_trace(const std::string &src_gpx_file, const vec<2> origin)
+{
+    this->origin = origin;
+    if (!load_gpx_file(src_gpx_file))
+    {
+        std::cout << "gps_trace() - Error while opening the gpx file";
+    }
+};
+
+bool gps_trace::load_gpx_file(const std::string& src) {
+    std::ifstream file(src);
+    if (!file.is_open()) {
+        std::cout << "load_gpx_file() - Failed to open file";
+        return false; // Failed to open file
+    }
+
+    // Save file to string stream for processing
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    std::string content = buffer.str();
+    file.close();
+
+    try {
+        rapidxml::xml_document<> doc;
+        doc.parse<0>(&content[0]);
+
+        rapidxml::xml_node<> *gpx_node = doc.first_node("gpx");
+        if (!gpx_node) { return false; }
+
+        rapidxml::xml_node<> *trk_node = gpx_node->first_node("trk");
+        if (!trk_node) { return false; }
+
+        for (rapidxml::xml_node<>* trkseg_node = trk_node->first_node("trkseg"); 
+        trkseg_node; 
+        trkseg_node = trkseg_node->next_sibling("trkseg")) {
+                
+            for (rapidxml::xml_node<>* trkpt_node = trkseg_node->first_node("trkpt"); 
+            trkpt_node; 
+            trkpt_node = trkpt_node->next_sibling("trkpt")) {
+
+                rapidxml::xml_attribute<>* lat = trkpt_node->first_attribute("lat");
+                rapidxml::xml_attribute<>* lon = trkpt_node->first_attribute("lon");
+
+                if (lat && lon) {
+                    vec<2> pos;
+                    if (track.empty()) {
+                        pos = make_vec(0.0, 0.0);
+                    } else {
+                        pos = coord_to_meters(std::stod(lat->value()), std::stod(lon->value()), track[0].lat, track[0].lon);
+                    }
+                    
+                    trkpt point;
+                    point.lat = pos[0];
+                    point.lon = pos[1];
+
+                    track.push_back(point);
+                    print_trkpt(point);
+                }
+            }
+        }
+
+    } catch (const rapidxml::parse_error& e) {
+        return false;
+    } catch (const std::exception& e) {
+        return false;
+    }
+
+    return !track.empty(); // Return true if at least one point was loaded
+}
+
+void gps_trace::print_trkpt(trkpt t) {
+    std::cout << std::setprecision(17);
+    std::cout << t.lat << " - " << t.lon << std::endl;
+}
+
+vec<2> gps_trace::coord_to_meters(double lat, double lon, double ref_lat, double ref_lon) {
+    const double EARTH_RADIUS = 6371000.0;
+
+    // todo: update PI with appropriate value
+    //calculates the distance between points
+    double d_lat = (lat * 3.14 / 180.0) - (ref_lat * 3.14 / 180.0);
+    double d_lon = (lon * 3.14 / 180.0) - (ref_lon * 3.14 / 180.0);
+
+    double x = EARTH_RADIUS * std::cos(ref_lat) * d_lon;
+    double y = EARTH_RADIUS * d_lat;
+
+    return make_vec(x, y);
+}
+
+}
+}
--- src/lib/gps/gps_trace.hpp
+++ src/lib/gps/gps_trace.hpp
@@ -1,6 +1,6 @@
 
-#ifndef FCPP_COMPONENT_GPS_TRACE_H_
-#define FCPP_COMPONENT_GPS_TRACE_H_
+#define _USE_MATH_DEFINES
+#include <cmath>
 
 #include <string>
 #include <vector>
@@ -9,6 +9,8 @@
 #include <stdexcept>
 #include <iostream>
 #include <iomanip>
+#include "../data/vec.hpp"  // Updated to use the correct relative path
+
 
 #include "rapidxml.hpp" // RapidXML header
 
@@ -30,7 +32,6 @@ class gps_trace {
             double lon;
 
             //todo: add timestamp management
-
             //todo: trkpt can also have elevation from <ele> child
         };
 
@@ -40,11 +41,7 @@ class gps_trace {
          * 
          * @param src_gpx_file The src of the gpx file to load
          */
-        gps_trace(const std::string& src_gpx_file) {
-            if (!load_gpx_file(src_gpx_file)) {
-                std::cout << "gps_trace() - Error while opening the gpx file";
-            }
-        };
+        gps_trace(const std::string& src_gpx_file, const vec<2> origin);
 
 
         /**
@@ -52,70 +49,24 @@ class gps_trace {
          * 
          * @param src The src of the gpx file to load
          */
-        bool load_gpx_file(const std::string& src) {
-            std::ifstream file(src);
-            if (!file.is_open()) {
-                std::cout << "load_gpx_file() - Failed to open file";
-                return false; // Failed to open file
-            }
+        bool load_gpx_file(const std::string& src);
 
-            // Save file to string stream for processing
-            std::stringstream buffer;
-            buffer << file.rdbuf();
-            std::string content = buffer.str();
-            file.close();
 
-            try {
-                rapidxml::xml_document<> doc;
-                doc.parse<0>(&content[0]);
+        /**
+         * @brief print a trkpt lat and lon in the console
+         */
+        void print_trkpt(trkpt t);
 
-                rapidxml::xml_node<> *gpx_node = doc.first_node("gpx");
-                if (!gpx_node) { return false; }
 
-                rapidxml::xml_node<> *trk_node = gpx_node->first_node("trk");
-                if (!trk_node) { return false; }
-
-                for (rapidxml::xml_node<>* trkseg_node = trk_node->first_node("trkseg"); 
-                trkseg_node; 
-                trkseg_node = trkseg_node->next_sibling("trkseg")) {
-                        
-                    for (rapidxml::xml_node<>* trkpt_node = trkseg_node->first_node("trkpt"); 
-                    trkpt_node; 
-                    trkpt_node = trkpt_node->next_sibling("trkpt")) {
-                        
-                        trkpt point;
-                        rapidxml::xml_attribute<>* lat = trkpt_node->first_attribute("lat");
-                        rapidxml::xml_attribute<>* lon = trkpt_node->first_attribute("lon");
-
-                        if (lat && lon) {
-                            point.lat = std::stod(lat->value());
-                            point.lon = std::stod(lon->value());
-
-                            track.push_back(point);
-                            print_trkpt(point);
-                        }
-                    }
-                }
-
-            } catch (const rapidxml::parse_error& e) {
-                return false;
-            } catch (const std::exception& e) {
-                return false;
-            }
-
-            return !track.empty(); // Return true if at least one point was loaded
-        }
-
-        void print_trkpt(trkpt t) {
-            //std::cout << std::setprecision(17);
-            std::cout << t.lat << " - " << t.lon << std::endl;
-        }
+        /**
+         * @brief Haversine formula to convert lat/lon to meters
+         */
+        vec<2> coord_to_meters(double lat, double lon, double ref_lat, double ref_lon);
 
     private:
         std::vector<trkpt> track;
+        vec<2> origin;
 };
 
 }
 }
-
-#endif // FCPP_COMPONENT_GPS_TRACE_H_
